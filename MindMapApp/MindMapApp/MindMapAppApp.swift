e3605007a0251033298539929236b30f9493d1cd
Faycel2015 <faycel.ayech@gmail.com>
2025-01-14 19:58:56 +0100

    Initial commit

---
+++ MindMapApp/MindMapApp/MindMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMapAppApp.swift
+//  MindMapApp
+//
+//  Created by FayTek on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct MindMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
